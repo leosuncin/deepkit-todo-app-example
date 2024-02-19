8954e1c78da6764ed331a6069b152681ee9bbafc
Jaime Leonardo Suncin Cruz <leosuncin@users.noreply.github.com>
2024-02-19 17:50:48 +0000

    feat(todo-list): add service for tasks
    Store tasks in-memory only
    Implement unit tests

---
+++ src/app/task.service.spec.ts
@@ -0,0 +1,106 @@
+import { describe, expect, it } from '@jest/globals';
+
+import { TaskService } from './task.service';
+
+describe('TaskService', () => {
+  it('be defined', () => {
+    expect(new TaskService()).toBeDefined();
+  });
+
+  it('create a task with a valid title and add it to the task list', () => {
+    const service = new TaskService();
+    const taskTitle = 'Valid Task Title';
+    const task = service.create(taskTitle);
+
+    expect(task).toEqual({
+      id: expect.any(String),
+      title: taskTitle,
+      completed: false,
+    });
+  });
+
+  it('get a task by its id and return the correct task', () => {
+    const taskService = new TaskService();
+    const taskTitle = 'Valid Task Title';
+    const createdTask = taskService.create(taskTitle);
+
+    const retrievedTask = taskService.get(createdTask.id);
+
+    expect(retrievedTask).toBe(createdTask);
+  });
+
+  it('get all tasks and return all tasks in the task list', () => {
+    const taskService = new TaskService();
+    const taskTitle1 = 'Task 1';
+    const taskTitle2 = 'Task 2';
+    const createdTask1 = taskService.create(taskTitle1);
+    const createdTask2 = taskService.create(taskTitle2);
+
+    const allTasks = taskService.getAll();
+
+    expect(allTasks).toContain(createdTask1);
+    expect(allTasks).toContain(createdTask2);
+  });
+
+  it('get a task with an invalid id and return undefined', () => {
+    const taskService = new TaskService();
+    const invalidId = 'invalid-id';
+
+    const retrievedTask = taskService.get(invalidId);
+
+    expect(retrievedTask).toBeUndefined();
+  });
+
+  it.each([{ title: 'New title' }, { completed: true }])(
+    'update a task with %j when given a valid id',
+    (changes) => {
+      const taskService = new TaskService();
+      const task = taskService.create('Task 1');
+
+      const updatedTask = taskService.update(task.id, changes);
+
+      expect(updatedTask).toBeDefined();
+      expect(updatedTask).toMatchObject(changes);
+    },
+  );
+
+  it('not update a task when given an invalid id', () => {
+    const taskService = new TaskService();
+    const invalidId = 'invalid-id';
+    const newTitle = 'New title';
+
+    const updatedTask = taskService.update(invalidId, { title: newTitle });
+
+    expect(updatedTask).not.toBeDefined();
+  });
+
+  it('delete an existing task by its id', () => {
+    const taskService = new TaskService();
+    const task = taskService.create('Task 1');
+
+    expect(taskService.getAll()).toHaveLength(1);
+    expect(taskService.delete(task.id)).toBe(true);
+    expect(taskService.getAll()).toHaveLength(0);
+    expect(taskService.get(task.id)).toBeUndefined();
+  });
+
+  it('return false when trying to delete a non-existing task', () => {
+    const taskService = new TaskService();
+    const result = taskService.delete('non-existing-id');
+
+    expect(result).toBe(false);
+  });
+
+  it('clear the list of tasks when called', () => {
+    const taskService = new TaskService();
+    taskService.create('Task 1');
+    taskService.create('Task 2');
+    taskService.create('Task 3');
+
+    expect(taskService.getAll()).toHaveLength(3);
+
+    taskService.deleteAll();
+
+    expect(taskService.getAll()).toHaveLength(0);
+  });
+});
---
+++ src/app/task.service.ts
@@ -0,0 +1,52 @@
+import { MinLength, Partial, UUID, uuid } from '@deepkit/type';
+
+export interface Task {
+  id: UUID;
+  title: string & MinLength<3>;
+  completed: boolean;
+}
+
+export class TaskService {
+  #tasks = new Map<UUID, Task>();
+
+  create(title: Task['title']): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      completed: false,
+    };
+
+    this.#tasks.set(task.id, task);
+
+    return task;
+  }
+
+  get(id: UUID): Task | undefined {
+    return this.#tasks.get(id);
+  }
+
+  getAll(): Task[] {
+    return Array.from(this.#tasks.values());
+  }
+
+  update(id: UUID, changes: Partial<Omit<Task, 'id'>>): Task | undefined {
+    const task = this.get(id);
+
+    if (task) {
+      task.title = changes.title ?? task.title;
+      task.completed = changes.completed ?? task.completed;
+
+      this.#tasks.set(id, task);
+    }
+
+    return task;
+  }
+
+  delete(id: UUID): boolean {
+    return this.#tasks.delete(id);
+  }
+
+  deleteAll(): void {
+    this.#tasks.clear();
+  }
+}
