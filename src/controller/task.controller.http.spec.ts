350d216ffa73a4d62fddea3a3f2282ce472b7133
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2024-02-29 19:23:09 -0600

    test(todo-list): fix CRUD with an HTTP controller
    Mock the database

--- src/controller/task.controller.http.spec.ts
+++ src/controller/task.controller.http.spec.ts
@@ -1,127 +1,90 @@
 import { createTestingApp } from '@deepkit/framework';
 import { HttpRequest } from '@deepkit/http';
+import { Logger, MemoryLoggerTransport } from '@deepkit/logger';
+import { Database, MemoryDatabaseAdapter } from '@deepkit/orm';
 import { describe, expect, it } from '@jest/globals';
 
 import { TaskParameterResolver } from '../app/task-parameter.resolver';
+import { Task } from '../app/task.entity';
 import { TaskService } from '../app/task.service';
 import { TaskControllerHttp } from './task.controller.http';
 
 describe('TaskControllerHttp', () => {
-  it('create a task', async () => {
-    const testing = createTestingApp({
+  let testing: ReturnType<typeof createTestingApp>;
+
+  beforeEach(async () => {
+    testing = createTestingApp({
       controllers: [TaskControllerHttp],
-      providers: [TaskService, TaskParameterResolver],
+      providers: [
+        {
+          provide: Logger,
+          useValue: new Logger([new MemoryLoggerTransport()]),
+        },
+        {
+          provide: Database,
+          useFactory: () => new Database(new MemoryDatabaseAdapter(), [Task]),
+        },
+        TaskService,
+        TaskParameterResolver,
+      ],
     });
 
     await testing.startServer();
+  });
 
-    try {
-      const response = await testing.request(
-        HttpRequest.POST('/tasks').json({ title: 'New Task' }),
-      );
+  afterEach(async () => {
+    await testing.stopServer();
+  });
 
-      expect(response.statusCode).toBe(200);
-      expect(response.json).toMatchObject({ title: 'New Task' });
-    } finally {
-      await testing.stopServer();
-    }
+  it('create a task', async () => {
+    const response = await testing.request(
+      HttpRequest.POST('/tasks').json({ title: 'New Task' }),
+    );
+
+    expect(response.statusCode).toBe(200);
+    expect(response.json).toMatchObject({ title: 'New Task' });
   });
 
   it('list all tasks', async () => {
-    const testing = createTestingApp({
-      controllers: [TaskControllerHttp],
-      providers: [TaskService, TaskParameterResolver],
-    });
+    const response = await testing.request(HttpRequest.GET('/tasks'));
 
-    await testing.startServer();
-
-    try {
-      const response = await testing.request(HttpRequest.GET('/tasks'));
-
-      expect(response.statusCode).toBe(200);
-      expect(response.json).toMatchObject([]);
-    } finally {
-      await testing.stopServer();
-    }
+    expect(response.statusCode).toBe(200);
+    expect(response.json).toMatchObject([]);
   });
 
   it('delete all tasks', async () => {
-    const testing = createTestingApp({
-      controllers: [TaskControllerHttp],
-      providers: [TaskService, TaskParameterResolver],
-    });
+    const response = await testing.request(HttpRequest.DELETE('/tasks'));
 
-    await testing.startServer();
-
-    try {
-      const response = await testing.request(HttpRequest.DELETE('/tasks'));
-
-      expect(response.statusCode).toBe(204);
-    } finally {
-      await testing.stopServer();
-    }
+    expect(response.statusCode).toBe(204);
   });
 
   it('get one task', async () => {
-    const testing = createTestingApp({
-      controllers: [TaskControllerHttp],
-      providers: [TaskService, TaskParameterResolver],
-    });
+    const { id } = await testing.app.get(TaskService).create('New Task');
+    const response = await testing.request(HttpRequest.GET(`/tasks/${id}`));
 
-    await testing.startServer();
-
-    try {
-      const { id } = testing.app.get(TaskService).create('New Task');
-      const response = await testing.request(HttpRequest.GET(`/tasks/${id}`));
-
-      expect(response.statusCode).toBe(200);
-    } finally {
-      await testing.stopServer();
-    }
+    expect(response.statusCode).toBe(200);
   });
 
   it('update one task', async () => {
-    const testing = createTestingApp({
-      controllers: [TaskControllerHttp],
-      providers: [TaskService, TaskParameterResolver],
+    const { id, completed } = await testing.app
+      .get(TaskService)
+      .create('New Task');
+    const response = await testing.request(
+      HttpRequest.PATCH(`/tasks/${id}`).json({ title: 'Updated Task' }),
+    );
+
+    expect(response.statusCode).toBe(200);
+    expect(response.json).toMatchObject({
+      id,
+      completed,
+      title: 'Updated Task',
     });
-
-    await testing.startServer();
-
-    try {
-      const { id, completed } = testing.app.get(TaskService).create('New Task');
-      const response = await testing.request(
-        HttpRequest.PATCH(`/tasks/${id}`).json({ title: 'Updated Task' }),
-      );
-
-      expect(response.statusCode).toBe(200);
-      expect(response.json).toMatchObject({
-        id,
-        completed,
-        title: 'Updated Task',
-      });
-    } finally {
-      await testing.stopServer();
-    }
   });
 
   it('delete one task', async () => {
-    const testing = createTestingApp({
-      controllers: [TaskControllerHttp],
-      providers: [TaskService, TaskParameterResolver],
-    });
+    const { id } = await testing.app.get(TaskService).create('New Task');
+    const response = await testing.request(HttpRequest.DELETE(`/tasks/${id}`));
 
-    await testing.startServer();
-
-    try {
-      const { id } = testing.app.get(TaskService).create('New Task');
-      const response = await testing.request(
-        HttpRequest.DELETE(`/tasks/${id}`),
-      );
-
-      expect(response.statusCode).toBe(204);
-    } finally {
-      await testing.stopServer();
-    }
+    expect(response.statusCode).toBe(204);
   });
 });
